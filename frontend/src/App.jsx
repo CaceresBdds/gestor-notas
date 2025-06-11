6eba5f269aae2aa7d1dbbb1788ac6185bb17fa1f
BenjaDev <bcaceres.dds@gmail.com>
2025-06-10 22:40:30 -0300

    importo el componente Notas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from 'react';
+import Notas from "./Notas";
 
 function App() {
-  const [notas, setNotas] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/api/notas')
-    .then((res) => res.json())
-    .then((data) => setNotas(data))
-    .catch((err) => console.error('Error al obtener notas:', err));
-  }, []);
-
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Notas</h1>
-      {notas.length === 0 ? (
-        <p>No hay notas disponibles.</p>
-      ):(
-        <ul>
-          {notas.map((nota) => (
-            <li key={nota.id}>
-              <strong>{nota.titulo}</strong><br />
-              <span>{nota.contenido}</span>
-            </li>
-          ))}
-        </ul>
-      )}
+      <h1>Gestor de Notas</h1>
+      <Notas />
     </div>
-  )
+  );
 }
 
 export default App;
